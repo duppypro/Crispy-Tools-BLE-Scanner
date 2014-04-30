fde62ebbb5f605e92108e7b42b92e35ee600a2ab
Duppy <duppy@duppy.com>
2014-04-30 12:09:50 -0700

    FIRST commit - not working

---
+++ ThingApp/electricimp/old/Crispy_BLE_Agent.nut
@@ -0,0 +1,187 @@
+/* crispy */
+/* CloudApp Agent Electric Imp Squirrel code */
+
+/////////////////////////////////////////////////
+// global constants and variables
+
+// generic
+const versionString = "crispy BLE v00.01.2014-04-29c"
+fakeMillis <- 42 // fake out millisecond times for debugging
+
+///////////////////////////////////////////////
+// constants for Firebase
+/*
+
+TODO: have this code self describe ThingInfo and StreamInfo by changing cloud to start ThingUuid@ttttttttttttt and StreamUuid@ttttttttttttt
+Change security rules to only enable writes for current time (- some skew slop)
+
+https://crispy-thingstream-name-server.firebaseio.com/
+    allStreamInfoByUuid
+
+https://crispy-thingstream-sessions.firebaseio.com/
+    allSessionsByStreamUuid/
+        2A33BB04-A035-45C8-827F-868D625BFE71-570f5ba6-963d-43e0-9af4-f91a47f0d077/
+            1380061438004
+
+https://crispy-thingstream-bigdata.firebaseio.com/
+    allDataBySessionRef/
+        crispy-impv00-hDzxieGblkLa-scanResponseBLE/0000000000000
+
+*/
+
+// which firebase to use
+const refNameServerRoot = "https://crispy-thingstream-name-server.firebaseio.com/"
+const refBigDataRoot = "https://crispy-thingstream-bigdata.firebaseio.com/allDataBySessionRef/" // FIXME: read this from Stream Info
+const refAllSessionsByStreamUuid = "https://crispy-thingstream-name-server.firebaseio.com/allSessionsUrl/allSessionsByStreamUuid/" // FIXME: read this with GET from https://crispy-thingstream-name-server.firebaseio.com/allStreamInfoByUuid/crispy-impv00-hDzxieGblkLa-scanResponseBLE/allSessionsRef/.json
+const uuidPrefix = "crispy-impv00-" // add prefix so that crispy UUIDs do not collide with other Thing UUIDs
+myThingInfo <- {}
+    myThingInfo.readme <- "crispy\r\nThis is a crispy HW BLE scanner."
+
+// helper variables ???
+
+///////////////////////////////////////////////
+//define functions
+
+//generic
+function timestamp() {
+    local dateNow = date()
+    return format("%010d%03d", dateNow.time, dateNow.usec / 1000)
+}
+
+// firebase specific
+function putStreamValue(table, timeKey) {
+    foreach (key, obj in table) {
+        if (key in refBigDataByName) {
+            // server.log("PUT " + key + "/" + timeKey + "/.json")
+            // server.log("PUT " + refBigDataByName[key] + "/" + timeKey + "/.json")
+            // server.log(http.jsonencode(obj))
+            http.request(
+                "PUT",
+                refBigDataByName[key] + "/" + timeKey + "/.json",
+                {},
+                http.jsonencode(obj)
+            ).sendasync(checkHttpRequestResponse)
+        } else {
+            if (key != "t") {
+                server.error("Unknown key : " + key)
+            }
+        }
+    }
+}
+
+http.onrequest(function(req,res) {
+    local timeKey = timestamp()
+    local table = req.query
+    local responseString = "PUT Stream Values :"
+
+    server.log("@" + timeKey + " REST REQUEST " + http.jsonencode(table))    
+    putStreamValue(table, timeKey)
+    responseString += "\r\n-------------------\r\n"
+    foreach (key, obj in table) {
+        if (key in refBigDataByName) {
+            responseString += "\tcurl -X PUT -d " + http.jsonencode(obj) + " "
+            responseString += refBigDataByName[key] + "/" + timeKey + "/.json"
+        } else {
+            responseString += "\tUnknown key : " + key
+        }
+        responseString += "\r\n-------------------\r\n"
+    }
+    res.send(200, responseString)
+})
+
+device.onconnect(function() {
+    server.log("Connect")
+    putStreamValue(
+        {
+            "healthStatus" : "[Agent detected device CONNECTED]",
+        },
+        timestamp()
+    )
+})
+
+device.ondisconnect(function() { // FIXME: send info to healthStatus when disconnect 
+    // BUGBUG: Agent does not detect device disconnect right away.
+    server.log("Disconnect")
+    putStreamValue(
+        {
+            "healthStatus" : "[Agent detected device *DIS*CONNECTED]",
+        },
+        timestamp()
+    )
+})
+
+device.on("event", function(table) {
+    local timeKey = timestamp()
+    server.log("@" + timeKey + " Device sent\r\n" + http.jsonencode(table)) // || table) FIXME: is '|| table' needed?
+    // post to firebase
+    // putStreamValue(table, timeKey)
+    putStreamValue(table, table.t)
+})
+
+function checkHttpRequestResponse(m) {
+    if (m.statuscode == 200) {
+        // server.log("Request Complete : " + m.body)
+    } else if (m.statuscode == 201) {
+        server.log("non-error statuscode 201 : " + m.body)
+    } else {
+        server.error("REQUEST error " + m.statuscode + "\r\n" + m.body)
+    }
+}
+ 
+////////////////////////////////////////////////////////
+// first Agent code starts here
+server.log("BOOTING_________ : " + versionString)
+server.log("Imp Agent URL___ : " + http.agenturl())
+server.log("Agent SW version : " + imp.getsoftwareversion())
+impAgentURLRoot <- http.agenturl()
+impAgentURLRoot = impAgentURLRoot.slice(0, impAgentURLRoot.find("/", "https://".len()) + 1)
+timeSessionStart <- timestamp()
+server.log("refBigDataRoot__________ : " + refBigDataRoot)
+thingUuid <- uuidPrefix + http.agenturl().slice(impAgentURLRoot.len())
+server.log("thingUuid_______________ : " + thingUuid)
+server.log("timeSessionStart________ : " + timeSessionStart)
+streamUuidByName <- {
+    "scanResponseBLE" : "scanResponseBLE-" + thingUuid,
+    "healthStatus" : "healthStatus-" + thingUuid,
+}
+refBigDataByName <- {
+    "scanResponseBLE" : refBigDataRoot + streamUuidByName["scanResponseBLE"] + "/" + timeSessionStart,
+    "healthStatus" : refBigDataRoot + streamUuidByName["healthStatus"] + "/" + timeSessionStart,
+}
+refAllSessionsByName <- {
+    "scanResponseBLE" : refAllSessionsByStreamUuid + streamUuidByName["scanResponseBLE"] + "/" + timeSessionStart,
+    "healthStatus" : refAllSessionsByStreamUuid + streamUuidByName["healthStatus"] + "/" + timeSessionStart,
+}
+// initialize new sessions for each out Stream
+foreach (key, val in refAllSessionsByName) {
+    server.log("PUT " + val + "/.json")
+    server.log(
+        http.jsonencode({
+            "__REF__" : refBigDataByName[key]
+        })
+    )
+    checkHttpRequestResponse(
+        http.request(
+            "PUT",
+            val + "/.json",
+            {},
+            http.jsonencode({
+                "__REF__" : refBigDataByName[key]
+            })
+        ).sendsync()
+    )
+}
+
+// send boot message
+putStreamValue(
+    {
+        "healthStatus" : "[BOOTING Electric Imp Agent] " + versionString,
+    },
+    timestamp()
+)
+
+// No more code to execute so we'll wait for messages from Device code.
+// End of code.
+
+// crispy hardware
+// CloudApp Electric Imp Agent Squirrel code
---
+++ WebApp/CrispyToolsBLE.js
@@ -0,0 +1,433 @@
+(function Viz() {
+'use strict'
+///////////////////////////////////////////////////////////////////////////////
+// live viewer of BLE scanner ouput.  BLE scanner logs to firebase ThingStreams
+///////////////////////////////////////////////////////////////////////////////
+
+// human readable version of this script
+var scriptVersion = 'CrispyToolsBLE.js v2014-04-30a'
+
+///////////////////////////////////////////////////////////////////////////////
+// firebase references
+///////////////////////////////////////////////////////////////////////////////
+var refThingstreamServer = 'https://crispy-thingstream-name-server.firebaseio.com' // URL of the firebase
+var d3SelViz = d3.selectAll('#Viz') // DOM element that holds all visualizations
+var d3SelLog = d3.selectAll('.pageLog') // DOM element for log messages
+var d3SelButtons // DOM element for Buttons
+var d3SelThingVizs // DOM element for all ThingStream Visualizations
+var TEXT_HEIGHT = 16 // default guess, this is updated later in page load
+var estimatedServerTimeOffset = 0 // milliseconds
+var refServerTimeOffset = new Firebase(fbUrl([
+		refThingstreamServer, '.info', 'serverTimeOffset'
+	]))
+var refAllStreamInfoByUuid = new Firebase(fbUrl([
+		refThingstreamServer,	'allStreamInfoByUuid'
+	]))
+var refAllThingInfoByUuid = new Firebase(fbUrl([
+		refThingstreamServer,	'allThingInfoByUuid'
+	]))
+var allVizRefArraysByThingUuid = {} // save refs globally for cleanup when removed
+var allVizd3SelArraysByThingUuid = {} // save dom elements globally for cleanup when removed
+var debugLogVizInfo = { // TODO: get these *Info objects from cloud Viz name-server
+		limit : 50,
+		title : 'Debug Log from UART A (imp pin 7)',
+		VizName : 'debugLog',
+		VizTitle : 'debugLogTitle'
+	}
+var debugLogVizInfoB = { // TODO: get these *Info objects from cloud Viz name-server
+		limit : 50,
+		title : 'Debug Log from UART B (imp pin 9)',
+		VizName : 'debugLogB',
+		VizTitle : 'debugLogBTitle'
+	}
+
+function serverNow() {
+	return new Date().getTime() + estimatedServerTimeOffset
+}
+
+function fbUrl(pathArray) { // helper to construct URL from list of path parts
+	return pathArray.join('/')
+}
+
+function compareKey(keyName, sortOrder, returnObject) {
+	// sortOrder is >0 for ascending, <0 for descending (reversed), ==0 for no change defaults to 1
+	if (returnObject) {
+		returnObject.less = 0
+		returnObject.more = 0
+		returnObject.equal = 0
+	}
+	return function (d1, d2) {
+		var result = (sortOrder || 1) > 0 ? d1[keyName] - d2[keyName] : sortOrder < 0 ? d2[keyName] - d1[keyName] : 0
+		if (returnObject) {
+			result < 0 ? returnObject.less++
+				: result > 0 ? returnObject.more++
+					: returnObject.equal++  
+		}
+		return result
+	}
+}
+
+function millisToLocalTimeStringFriendly(t) { // helper
+	var s,
+		time,
+		months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
+	time = new Date(parseInt(String(t).substr(0,13),10))
+	s = //time.getFullYear() + 
+		//'-' + 
+		// String('00' + (time.getMonth() + 1)).slice(-2) + 
+		months[time.getMonth()] + 
+		'-' + String('00' + time.getDate()).slice(-2) + 
+		' at ' + String('00' + time.getHours()).slice(-2) + 
+		':' + String('00' + time.getMinutes()).slice(-2) + 
+		':' + String('00' + time.getSeconds()).slice(-2) + 
+		// '.' + String('000' + t % 1000).slice(-3) + 
+		'.' + String(t).substr(10,6)
+		// ' (' + t + ')'
+	return s    
+}
+
+function appendThingName(d3Selection) {
+	d3Selection
+		.append('div')
+		.attr({
+			name : 'thingName',
+			class : 'panel-heading text-center'
+		})	
+}
+
+function appendList(d3Selection, info) {
+	var sel
+	sel = d3Selection
+		.append('div')
+		.attr({
+			class : 'col-6'
+		})
+	sel
+		.append('div')
+		.attr({
+			name : info.VizTitle,
+			class : 'col-6 text-center'
+		})
+	sel
+		.append('div')		
+		.attr({
+			name : info.VizName,
+			class : 'col-6',
+			style : 'resize:vertical;overflow:auto;height:248px'
+		})
+}
+
+function appendDebugLog(d3Selection) { // use with d3.call()
+	appendList(d3Selection, debugLogVizInfo)
+	d3Selection.select('[name=' + debugLogVizInfo.VizTitle + ']')
+		.text(debugLogVizInfo.title)	
+}
+
+function appendDebugLogB(d3Selection) { // use with d3.call()
+	appendList(d3Selection, debugLogVizInfoB)
+	d3Selection.select('[name=' + debugLogVizInfoB.VizTitle + ']')
+		.text(debugLogVizInfoB.title)	
+}
+
+function redrawList(d3Data, textFromD3Data) {
+	var d
+	var sessions
+
+	d = this.d3SelThing
+		.selectAll('[name=' + this.info.VizName + ']')
+
+	sessions = d
+		.selectAll('div')
+		.data(
+			d3Data,
+			function (d) {
+				return d.timeSessionStart
+			}
+		)
+
+	sessions.enter()
+		.append('div')
+		.attr({
+			style : "font:'Lucida Console';font-size:9pt;background:#000;color:#0cd"
+		})
+		.text(textFromD3Data)
+
+	sessions
+		.sort(compareKey('timeSessionStart'))
+
+	sessions.exit()
+		.remove()
+
+	d.node().scrollTop = d.node().scrollHeight
+}
+
+function redrawDebugLog(d3Data) { // called on data change
+	redrawList.call(this, d3Data, function (d) {
+			return millisToLocalTimeStringFriendly(d.timeSessionStart) + '> ' + d.string
+		})
+} //redrawDebugLog()
+
+function removeVizByThingUuid(thingUuid) {
+	// cancel all firebase callbacks that addVizFromThingUuid() created
+	for (var i = allVizRefArraysByThingUuid[thingUuid].length - 1; i >= 0; i--) {
+		allVizRefArraysByThingUuid[thingUuid][i].off()
+	}
+	delete allVizRefArraysByThingUuid[thingUuid]
+	// remove all dom elements that addVizFromThingUuid() created
+	for (var i = allVizd3SelArraysByThingUuid[thingUuid].length - 1; i >= 0; i--) {
+		allVizd3SelArraysByThingUuid[thingUuid][i].remove()
+	}
+	delete allVizd3SelArraysByThingUuid[thingUuid]
+}
+
+function convertFBSnapToD3Data(snap) {
+	var d3Data
+	var d3Data2
+	var v = snap.val()
+	var keyArray
+	var startTime
+
+	// d3Data = []
+	// startTime = new Date()
+	// for(var timeSessionStart in v) { // convert JSON to array for use by d3
+	// 	var obj = {}
+	// 	obj.string = v[timeSessionStart]
+	// 	// move timestamp form key name into object then push on the array
+	// 	obj.timeSessionStart = timeSessionStart // allready in absolute milliseconds
+	// 	d3Data.push(obj)
+	// }
+	// console.log('for(var ... in... ) = ' + (new Date() - startTime))
+
+	d3Data2 = []
+	// startTime = new Date()
+	keyArray = Object.keys(v)
+	for (var i = 0, l = keyArray.length; i < l; i++) {
+		var t = keyArray[i]
+		d3Data2.push({
+			string : v[t],
+			timeSessionStart : t
+		})
+	}
+	// console.log('Object.keys() = ' + (new Date() - startTime))
+
+	return d3Data2
+}
+
+function onValueDebugLog(snap) {
+	var d3Data = convertFBSnapToD3Data(snap)
+	redrawDebugLog.call(this, d3Data)
+}
+
+function addVizListFromThis() {
+	console.log('addVizListFromThis : ' + this.streamUuid)
+	refAllStreamInfoByUuid
+	.child(this.streamUuid)
+	.child('allSessionsRef')
+	.once(
+		'value',
+		function (snap) {
+			var ref
+			console.log('addVizListFromThis allSessionsRef : ' + snap.val())
+			ref = new Firebase(snap.val())
+			allVizRefArraysByThingUuid[this.thingUuid].push(ref)
+			ref
+			.endAt()
+			.limit(1)
+			.on(
+				'value',
+				function (snap) {
+					var obj = {}
+					var refAllItems
+					for(var key in snap.val()) { // convert JSON to array for use by d3
+						obj = snap.val()[key]
+					}
+					refAllItems = new Firebase(obj['__REF__'])
+					allVizRefArraysByThingUuid[this.thingUuid].push(refAllItems)
+					console.log("Match? " + this.streamUuid + " and " + this.onValueFunction)
+					refAllItems
+					.endAt()
+					.limit(this.info.limit)
+					.on(
+						'value',
+						this.onValueFunction,
+						function function_name (argument) {
+							// callback canceled...
+						},
+						this
+					)
+				},
+				function function_name (argument) {
+					// callback canceled...
+				},
+				this
+			)
+		},
+		function function_name (argument) {
+			// callback canceled...
+		},
+		this
+	)
+
+}
+
+function addVizFromThingUuid(thingUuid, info) {
+	var refAllOutStreamUuidsByName // firebase reference
+	var context = {}
+
+	allVizd3SelArraysByThingUuid[thingUuid] = [] // cache dom elements globally for later removal
+	allVizRefArraysByThingUuid[thingUuid] = [] // cache firebase callbacks gloablly for later removal
+
+	context.thingUuid = thingUuid // callbacks will need to know their thingUuid
+	// FIXME: is it better to get thingUuid from d3.select(this).attr('thingUuid') or from a global arrray instead?
+
+	// add Name
+	context.d3SelThing =
+		d3SelThingVizs // cache <div> element for this thingUuid
+			.append('div')
+			.attr({
+				thingUuid : thingUuid,
+				class : 'panel panel-default col-12'
+			})
+	allVizd3SelArraysByThingUuid[thingUuid].push(context.d3SelThing)
+	
+	// create dom elements for Viz widgets
+	context.d3SelThing
+		.style('cursor', 'default')
+		.call(appendThingName)
+
+	// fill the thingUuidName widget
+	context.d3SelThing.select('[name=thingName]')
+		.text(info.name || thingUuid)
+
+	refAllOutStreamUuidsByName = refAllThingInfoByUuid.child(thingUuid).child('allOutStreamUuidsByName')
+	allVizRefArraysByThingUuid[thingUuid].push(refAllOutStreamUuidsByName)
+
+	refAllOutStreamUuidsByName
+	.once(
+		'value',
+		function (snap) {
+			for (var name in snap.val()) {
+				this.streamUuid = snap.val()[name]
+				if (name == 'debugLog') {
+					this.d3SelThing
+						.call(appendDebugLog)
+					addVizListFromThis.call({
+						d3SelThing : this.d3SelThing,
+						thingUuid : this.thingUuid,
+						streamUuid : this.streamUuid,
+						onValueFunction : onValueDebugLog,
+						info : debugLogVizInfo
+					})
+				}
+				if (name == 'debugLogB') {
+					this.d3SelThing
+						.call(appendDebugLogB)
+					addVizListFromThis.call({
+						d3SelThing : this.d3SelThing,
+						thingUuid : this.thingUuid,
+						streamUuid : this.streamUuid,
+						onValueFunction : onValueDebugLog,
+						info : debugLogVizInfoB
+					})
+				}
+			}
+		},
+		function cancelCallback() {
+			console.log('refAllOutStreamUuidsByName callback canceled')
+		},
+		context
+	)
+
+	// delete context.recentDebugLogs // FIXME: Why did I need this?  I forgot.
+} // addVizFromThingUuid(...
+
+// code starts here
+logD3Sel.append('div').text(scriptVersion)
+
+lineHeightGLobal = parseInt(logD3Sel.select('div').style('line-height'), 10) - 2 // HACK: '- 2' is a fudge
+// FIXME: sometimes on page load the height is stll 0.  Maybe make an on resize event for this div to reset lineHeightGLobal?
+console.log("lineHeightGLobal set to " + lineHeightGLobal)
+
+logD3Sel.append('div').text('Loading Firebase...')
+// DEPLOY: comment out this line before deploying
+// Firebase.enableLogging(true)
+
+// Start getting data from Firebase here
+// These firebase refs exist as long as the page is loaded.
+// No need to save them for later .off() calls
+	// serverTimeOffsetRef - used for time sync.  could be better
+	// refAllThingInfoByUuid
+	
+serverTimeOffsetRef.on('value', function (snap) {
+// minimal effort here to sync local time and server time.
+	var d3Sel = logD3Sel.selectAll('.fbLoadMessage')
+		// .data([true]) // FIXME: is there a better way?
+
+	// data here
+	serverTimeOffset = snap.val() // change global time offset
+	
+	// presentation part here.  TODO: seperate data and presentation?
+	d3Sel.enter()	
+		.append('div').attr({ class : 'fbLoadMessage' })
+	d3Sel
+		// .style({'background-color': firebaseColor.changed})
+		.style({'background-color' : firebaseColor.added})
+		.text('>Firebase Loaded. (serverTimeOffset = '
+			+ serverTimeOffset + 'ms)')
+		.transition().ease('linear').duration(333)
+		.style({'background-color' : 'white'})
+})
+
+refAllThingInfoByUuid.on(
+	'value',
+	function(snap) {
+		// clear existing
+		d3SelViz.selectAll('*').remove()
+		d3SelButtons = d3SelViz
+			.append('div')
+			.attr({
+				class : 'panel panel-default col-12'
+			})
+			.append('div')
+			.attr({
+				class : 'panel-body btn-group',
+				name : 'buttonContainer'
+			})
+		d3SelThingVizs = d3SelViz
+			.append('div')
+			.attr({
+				name : 'thingUuidContainer'
+			})
+
+		// garbage collect timers and FB refs
+			// FIXME: TODO garbage collection?
+		// add new Viz for each thingUuid	
+		for (var thingUuid in snap.val()) {
+			var info = snap.val()[thingUuid]
+			d3SelButtons
+				.append('button')
+				.attr({
+					thingUuid : thingUuid,
+					style : 'margin : 0px 2px;',
+					class : 'btn-small col-4'
+				})
+				.text(info.name || thingUuid)
+				.on('click', function() {
+					var thingUuid = d3.select(this).attr('thingUuid')
+					var info = snap.val()[thingUuid] // FIXME: how is snap included in context when referenced but not when not referenced and why is info not included in context
+					console.log('clicked button thingUuid = ' + thingUuid)
+					if (this.classList.toggle('btn-primary')) {
+						// create new Viz dom elements and fb callbacks
+						addVizFromThingUuid(thingUuid, info)
+					} else {
+						removeVizByThingUuid(thingUuid)
+					}
+				})
+		}
+	}
+)//refAllThingInfoByUuid.on('value'...
+
+}) ()
+
+// end of code
